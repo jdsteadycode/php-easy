663381c2d7f06f47f5cf60632fd8234aa9245195
jdsteadycode <desaijeet11@gmail.com>
2025-12-15 17:40:09 +0530

    added create and read problemset apis

---
+++ scripts/admin/apis/api.problemSets.js
@@ -0,0 +1,105 @@
+// A central object to handle API..
+export const ProblemSetsApi = {
+
+    // () -> get all the problemsets..
+    getProblemSets: async function() {
+
+        // make an php api call..
+        return fetch(
+            "http://localhost:8888/php_easy/apis/admin/problem_set/all_problem_sets.php",
+            {
+                "method": "GET",
+            }
+        )
+        // grab the response..
+        .then(function (response) {
+
+            // when response is not successful.. 
+            if(!response.ok) return {"status": false, "message": "http_error"};
+
+            // return response.text();
+
+            // get the response json parsed..
+            return response.json();
+        })
+        // get the parsed json data..
+        // .then(function (data) {
+
+        //     // check log..
+        //     // console.log(data);
+
+        //     // get the data..
+        //     return data;
+        // })
+        // when error arises..
+        .catch(function (error) {
+
+            // check log..
+            console.log(error);
+
+            // when request fails..
+            return {"status": false, "message": "network_error"};
+        });
+    },
+
+    // () -> add the problemSet along with the topics..
+    addProblemSetsAndTopics: async function(
+        title, 
+        description, 
+        difficulty, 
+        topicIds = [], 
+        sampleInput, 
+        sampleOutput, 
+        hintsText, 
+        createdBy) {
+        
+        // set the data to send..
+        const data = new FormData();
+        data.append("title", title);
+        data.append("description", description);
+        data.append("difficulty", difficulty);
+        data.append("topicIds", JSON.stringify(topicIds));
+        data.append("sampleInput", sampleInput);
+        data.append("sampleOutput", sampleOutput);
+        data.append("hintsText", hintsText);
+        data.append("created_by", createdBy);
+
+        // make an php api call..
+        return fetch(
+            "http://localhost:8888/php_easy/apis/admin/problem_set/add_problem_set.php",
+            {
+                "method": "POST",
+                "body": data
+            }
+        )
+        // handle the response..
+        .then(function (response) {
+
+            // when response is not successful.. 
+            if(!response.ok) return {"status": false, "message": "http_error"};
+
+            // return response.text();
+
+            // get the parsed json response..
+            return response.json();
+        })
+        // get the parsed json data..
+        // .then(function (data) {
+
+        //     // check log..
+        //     console.log(data);
+        // })
+        // re-render the topics
+        // .then(initManageTopics)
+
+        // when error arises..
+        .catch(function (error) {
+
+            // check log..
+            console.log(error);
+
+            // when request fails..
+            return {"status": false, "message": "network_error"};
+        });
+    }
+};
