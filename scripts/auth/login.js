d9a6ba58c03e90858ea2ca31dc538e66740a2b23
jdsteadycode <desaijeet11@gmail.com>
2025-12-15 17:44:55 +0530

    updated the authentication functionality by using modules

--- scripts/auth/login.js
+++ scripts/auth/login.js
@@ -1,3 +1,6 @@
+// grab the modules..
+import {AuthActions} from "../auth.js";
+
 // grab the html elements..
 const loginBtn = document.querySelector(".js-login-btn");
 
@@ -9,92 +12,8 @@ function login(email, password) {
     data.append("email", email);
     data.append("password", password);
 
-    // make an php api call..
-    fetch(
-        `http://localhost:8888/php_easy/apis/auth/login.php`,
-        {
-            "method": "POST",
-            "body": data,
-            "credentials": "include"    // send the cookie with each request..
-        }
-    )
-    // grab the response..
-    .then(function(response) {
-
-        // return response.text();
-        return response.json();
-    })
-    // sanitize the json response -> javascript native code..
-    .then(function(data) {
-
-        // check log..
-        console.log(data);
-
-        // when invalid credentials..
-        if(data.message === "invalid credentials") {
-
-            // show the toast..
-            document.querySelector(".js-error-msg").style.display = "block";
-            document.querySelector(".js-error-msg").innerText = data.message;
-
-            // just after 1.5 seconds..
-            setTimeout(function() {
-
-                // show the toast..
-                document.querySelector(".js-error-msg").innerText = "";
-                document.querySelector(".js-error-msg").style.display = "none";
-
-            }, 1500);
-            return;
-        }
-
-        // handle the redirection..
-        // according to the role..
-        if(data.message === "successful login" && data.status === true) {
-
-            // show the toast..
-            document.querySelector(".js-success-msg").innerText = ``;
-            document.querySelector(".js-success-msg").style.display = "block";
-            document.querySelector(".js-success-msg").innerText = data.message;
-
-
-            // after 1.5 seconds..
-            setTimeout(function() {
-
-                // clear the toast..
-                document.querySelector(".js-success-msg").innerText = ``;
-                document.querySelector(".js-success-msg").style.display = "none";
-
-
-                // handle the based on the role..
-                if(data.user_role === "admin") {
-
-                    // check for path..
-                    // console.log(window.location.origin);
-
-                    // redirect the admin..
-                    window.location.href = window.location.origin + `/php_easy/pages/admin/index.php`;
-                    return;
-                }
-
-                // coming soon for user..
-                document.querySelector(".js-success-msg").innerText = "coming soon for users.."
-                document.querySelector(".js-success-msg").style.display = "block";
-                
-
-
-            }, 1500);
-
-        }
-
-
-    })
-    // handle error
-    .catch(function(error) {
-
-        // check log..
-        console.log(error);
-    });
+    // make the user login..
+    AuthActions.loginTheUser(data);
 }
 
 // when login btn is clickd..
