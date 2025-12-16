1ca09962db648f0d1f13d3aca811ac5cbf968740
jdsteadycode <desaijeet11@gmail.com>
2025-12-16 17:40:55 +0530

    added problem sets functionality (add and update about 90%) and attached events

--- scripts/admin/apis/api.problemSets.js
+++ scripts/admin/apis/api.problemSets.js
@@ -101,5 +101,113 @@ export const ProblemSetsApi = {
             // when request fails..
             return {"status": false, "message": "network_error"};
         });
+    },
+
+    // () -> update the problemSet only..
+    updateProblemSet: async function(
+        title, 
+        description, 
+        difficulty, 
+        sampleInput, 
+        sampleOutput, 
+        hintsText, 
+        problemId) {
+        
+        // set the data to send..
+        const data = new FormData();
+        data.append("__method", "PATCH"); 
+        data.append("title", title);
+        data.append("description", description);
+        data.append("difficulty", difficulty);
+        data.append("sampleInput", sampleInput);
+        data.append("sampleOutput", sampleOutput);
+        data.append("hintstext", hintsText);
+        data.append("problemSetId", problemId);
+
+        // make an php api call..
+        return fetch(
+            "http://localhost:8888/php_easy/apis/admin/problem_set/update/update_problem_set.php",
+            {
+                "method": "POST",
+                "body": data
+            }
+        )
+        // handle the response..
+        .then(function (response) {
+
+            // when response is not successful.. 
+            if(!response.ok) return {"status": false, "message": "http_error"};
+
+            // return response.text();
+
+            // get the parsed json response..
+            return response.json();
+        })
+        // get the parsed json data..
+        // .then(function (data) {
+
+        //     // check log..
+        //     console.log(data);
+        // })
+        // re-render the topics
+        // .then(initManageTopics)
+
+        // when error arises..
+        .catch(function (error) {
+
+            // check log..
+            console.log(error);
+
+            // when request fails..
+            return {"status": false, "message": "network_error"};
+        });
+    },
+
+    // () -> new topics to problem..
+    assignNewTopicsToProblemSet: async function(problemSetId, topic_ids) {
+
+        // set the data to send..
+        const data = {
+            "problemSetId": problemSetId,
+            "topicIds": topic_ids
+        }
+
+        // make an php api call..
+        return fetch(
+            "http://localhost:8888/php_easy/apis/admin/problem_set/update/assign_topics.php",
+            {
+                "method": "POST",
+                "body": JSON.stringify(data)
+            }
+        )
+        // handle the response..
+        .then(function (response) {
+
+            // when response is not successful.. 
+            if(!response.ok) return {"status": false, "message": "http_error"};
+
+            // return response.text();
+
+            // get the parsed json response..
+            return response.json();
+        })
+        // get the parsed json data..
+        // .then(function (data) {
+
+        //     // check log..
+        //     console.log(data);
+        // })
+        // re-render the topics
+        // .then(initManageTopics)
+
+        // when error arises..
+        .catch(function (error) {
+
+            // check log..
+            console.log(error);
+
+            // when request fails..
+            return {"status": false, "message": "network_error"};
+        });
     }
 };
