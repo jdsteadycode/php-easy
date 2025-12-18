e94dbec2f914b4999402b01092020a92b71517b3
jdsteadycode <desaijeet11@gmail.com>
2025-12-18 17:33:21 +0530

    added apis for handling categories related tasks

---
+++ apis/admin/categories/add.php
@@ -0,0 +1,51 @@
+<?php
+    // handle the type of content for client..
+    header("Content-Type: application/json");
+
+    // important for sanitizing the current date and time..
+    // set the date and time.. (according to india)
+    date_default_timezone_set('Asia/Kolkata');
+
+    // grab modules..
+    require(dirname(__DIR__, 3) . "/config/config.php");
+
+    // get the Admin Controller module..
+    require(FILE_PATH . "/controllers/admin/CategoriesController.php");
+
+    // instantiate the class..
+    $controller = new CategoriesController();
+
+    // check the request method..
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        // get the data..
+        $incomingData = file_get_contents("php://input");
+
+        // sanitize the data from json  -> php native array..
+        $data = json_decode($incomingData, TRUE);
+
+        // handle new category addon
+        $response = $controller->handleAddCategory(
+            $data["name"],
+            $data["parentId"]
+        );
+
+        // set the response code..
+        http_response_code(200);
+
+        // check log..
+        echo json_encode($response);
+    
+    }
+    else {
+
+        // set the response code..
+        http_response_code(405);
+
+        // send response to client..
+        echo json_encode([
+            "message" => "invalid request method",
+            "status" => false
+        ]);
+        exit();
+    }
---
+++ apis/admin/categories/read.php
@@ -0,0 +1,45 @@
+<?php
+    // handle the type of content for client..
+    header("Content-Type: application/json");
+
+    // important for sanitizing the current date and time..
+    // set the date and time.. (according to india)
+    date_default_timezone_set('Asia/Kolkata');
+
+    // grab modules..
+    require(dirname(__DIR__, 3) . "/config/config.php");
+
+    // get the Admin Controller module..
+    require(FILE_PATH . "/controllers/admin/CategoriesController.php");
+
+    // instantiate the class..
+    $controller = new CategoriesController();
+
+    // check the request method..
+    if($_SERVER["REQUEST_METHOD"] == "GET") {
+
+        // handle new category addon
+        $response = $controller->handleAllCategories();
+
+        // set the response code..
+        http_response_code(200);
+
+        // check log..
+        echo json_encode([
+            "message" => "categories fetched successfully",
+            "data" => $response
+        ]);
+    
+    }
+    else {
+
+        // set the response code..
+        http_response_code(405);
+
+        // send response to client..
+        echo json_encode([
+            "message" => "invalid request method",
+            "status" => false
+        ]);
+        exit();
+    }
